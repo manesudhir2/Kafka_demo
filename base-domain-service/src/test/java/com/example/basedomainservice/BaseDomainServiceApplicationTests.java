168dba3941a9468969bd34b815bce66f054d23cc
Sudhir Alias Harshad Shrirang Mane <sudhiraliasharshadshrirangmane@Sudhirs-MacBook-Pro.local>
2022-12-08 21:42:53 +0530

    Kafka demo project first and final code

---
+++ base-domain-service/src/test/java/com/example/basedomainservice/BaseDomainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basedomainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ email-service/src/main/java/com/example/emailservice/kafka/OrderConsumer.java
@@ -0,0 +1,18 @@
+package com.example.emailservice.kafka;
+
+import com.example.basedomainservice.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consumer(OrderEvent orderEvent){
+        logger.info(String.format("OrderEvent received in EmailService => %s",orderEvent.toString()));
+    }
+}
